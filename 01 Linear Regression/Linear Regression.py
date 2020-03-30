dd35a7bb7295f4124c1d449938f6f87979a0fbe6
Jwoonge <511sjw@naver.com>
2020-03-30 17:18:39 +0900

    Linear Regression - visualization

--- 01 Linear Regression/Linear Regression.py
+++ 01 Linear Regression/Linear Regression.py
@@ -1,19 +1,15 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pylab as plt
 import math
 
-def original_function(input):
-    a = 2
-    b = 3
+def linear_function(x_range, a, b):
     ret = []
-    for i in range(len(input)):
-        y = a*input[i] + b
+    for i in range(len(x_range)):
+        y = a*x_range[i] + b
         ret.append(y)
     return ret
 
-def random_generation(x_range):
-    a = 2
-    b = 3
+def random_generation(x_range, a, b):
     ret = []
     n = np.random.randn(len(x_range))
     for i in range(len(x_range)):
@@ -32,44 +28,53 @@ def object_function(theta0, theta1, x_range, datas):
     print(ret)
     return ret
 
-def gradient_descent(theta0, theta1, x_s, y_s, learning_rate = 0.02):
+def gradient_descent(theta0, theta1, x_s, y_s, learning_rate = 0.03):
     update_theta0 = 0
     for i in range(len(x_s)):
         update_theta0 += (linear_model(theta0, theta1, x_s[i]) - y_s[i])/len(x_s)
-    #update_theta0 = round(update_theta0,20)
 
     update_theta1 = 0
     for i in range(len(x_s)):
         update_theta1 += ((linear_model(theta0, theta1, x_s[i]) - y_s[i]) * x_s[i])/len(x_s)
-    #update_theta1 = round(update_theta1,20)
 
     theta0_new = theta0 - learning_rate * update_theta0
     theta1_new = theta1 - learning_rate * update_theta1
 
     return theta0_new, theta1_new
 
-def optimization(theta0, theta1):
-    print("TODO")
-
-def convergence(theta0, theta1, t, convergence_rate = 0.01):
-    if theta0[t]==0 or theta1[t]==0:
+def convergence(theta0, theta1, t, convergence_rate = 0.000001):
+    if theta0[t-1]==0 or theta1[t-1]==0:
         return False
-    if np.abs((theta0[t+1]-theta0[t])/theta0[t]) < convergence_rate:
-        if np.abs((theta1[t+1]-theta1[t])/theta1[t]) < convergence_rate:
+    if np.abs((theta0[t]-theta0[t-1])/theta0[t-1]) < convergence_rate:
+        if np.abs((theta1[t]-theta1[t-1])/theta1[t-1]) < convergence_rate:
             return True
-    
     return False
 
-def convergence_stop(theta0, theta1, t):
-    if theta0[t]==theta0[t+1] and theta0[t-1]==theta0[t-2] and theta1[t]==theta1[t+1] and theta1[t-1]==theta1[t-2]:
-        return True
-    else:
-        return False
+def show_result():
+    plt.title("1_input_data")
+    plt.plot(x_range, datas, 'k.')
+    plt.plot(x_range, linear_function(x_range, a=a, b=b), 'b')
+    plt.show()
 
+    plt.title("2_output_results")
+    plt.plot(x_range, datas, 'k.')
+    plt.plot(x_range, linear_function(x_range, theta1[min_t], theta0[min_t]), 'r')
+    plt.show()
+
+    plt.title("3_energy values")
+    plt.plot(energy, 'b')
+    plt.show()
+
+    plt.title("4_model parameters")
+    plt.plot(theta0, 'r')
+    plt.plot(theta1, 'b')
+    plt.show()
+
+a=3
+b=-2
 m = 1000
-x_range = [0.01*i for i in range(1,m)]
-datas = random_generation(x_range)
-learning_rate = 0.2
+x_range = [0.01*i for i in range(m)]
+datas = random_generation(x_range, a=a, b=b)
 
 t=0
 theta0 = [0]
@@ -83,8 +88,10 @@ while True:
     theta1.append(theta1_new)
     t += 1
     energy.append(object_function(theta0[t],theta1[t],x_range,datas))
-    if convergence_stop(theta0, theta1,t-1):
+    if convergence(theta0, theta1,t):
         break
 
 min_t = energy.index(min(energy))
 print("t:",min_t,"  theta0:",theta0[min_t],"  theta1:",theta1[min_t])
+show_result()
+
